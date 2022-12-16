ff8ed8f38eb5f5ad705b33feca21c9b790b64dbe
Marcelo Peralta <hmarcelo.peralta@gmail.com>
2022-12-15 21:48:35 -0300

    cargando todo el proyecto final

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-const productos = [
+Swal.fire({
+    title: 'Bienvenido',
+    text: 'KHUMBU - Tienda de Montaña',
+    iconHtml:'<i class="bi bi-emoji-smile-fill"></i>'
+  })
+
+  const productos = [
     {
         id: "carpa-01",
         titulo: "Carpa 01",
@@ -7,7 +13,7 @@ const productos = [
             nombre: "Carpas",
             id: "carpas"
         },
-        precio: 10000
+        precio: " 50 "
     },
     {
         id: "carpa-02",
@@ -17,7 +23,7 @@ const productos = [
             nombre: "Carpas",
             id: "carpas"
         },
-        precio: 15000
+        precio: " 55"
     },
     {
         id: "carpa-03",
@@ -27,7 +33,7 @@ const productos = [
             nombre: "Carpas",
             id: "carpas"
         },
-        precio: 20000
+        precio: " 60"
     },
     {
         id: "carpa-04",
@@ -37,7 +43,7 @@ const productos = [
             nombre: "Carpas",
             id: "carpas"
         },
-        precio: 25000
+        precio: " 85"
     },
     {
         id: "calzado-01",
@@ -47,7 +53,7 @@ const productos = [
             nombre: "Calzados",
             id: "calzados"
         },
-        precio: 12000
+        precio: " 45"
     },
     {
         id: "calzado-02",
@@ -57,7 +63,7 @@ const productos = [
             nombre: "Calzados",
             id: "calzados"
         },
-        precio: 17000
+        precio: " 57"
     },
     {
         id: "calzado-03",
@@ -67,7 +73,7 @@ const productos = [
             nombre: "Calzados",
             id: "calzados"
         },
-        precio: 20000
+        precio: " 30"
     },
     {
         id: "calzado-04",
@@ -77,7 +83,7 @@ const productos = [
             nombre: "Calzados",
             id: "calzados"
         },
-        precio: 25000
+        precio: " 80"
     },
     {
         id: "mochila-01",
@@ -87,7 +93,7 @@ const productos = [
             nombre: "Mochilas",
             id: "mochilas"
         },
-        precio: 30000
+        precio: " 35"
     },
     {
         id: "mochila-02",
@@ -97,7 +103,7 @@ const productos = [
             nombre: "Mochilas",
             id: "mochilas"
         },
-        precio: 25000
+        precio: " 70"
     },
     {
         id: "mochila-03",
@@ -107,7 +113,7 @@ const productos = [
             nombre: "Mochilas",
             id: "mochilas"
         },
-        precio: 32000
+        precio: " 25"
     },
     {
         id: "mochila-04",
@@ -117,46 +123,120 @@ const productos = [
             nombre: "Mochilas",
             id: "mochilas"
         },
-        precio: 40000
+        precio: " 75"
     },
     
 ];
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+const Dolar = document.getElementById("Dolar");
 
-function cargarProductos(productosElegidos){
+const cargarDolar = () => {
+    fetch("https://criptoya.com/api/dolar")
+    .then(response => response.json()) 
+    .then(({solidario, blue, ccl, mep }) => {
+    console.log(solidario, blue, ccl, mep)
+    Dolar.innerHTML = `
+        <h2>Cotizacion del dolar </h2>
+        <p>Solidario: $ ${solidario}</p>
+        <p>Blue: $ ${blue}</p>
+        <p>CCL: $ ${ccl}</p>
+        <p>Mep: $ ${mep}</p>
+        
+    `
+}) 
+}
+cargarDolar()
+setInterval(() => {
+    cargarDolar()
+}, 60000);
+
+function cargarProductos(productosElegidos) {
 
     contenedorProductos.innerHTML = "";
 
     productosElegidos.forEach(producto => {
+
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
-        <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
-                    <div class="producto-detalles">
-                        <h3 class="producto-titulo">${producto.titulo}</h3>
-                        <p class="podructo-precio">${producto.precio}</p>
-                        <button class="producto-agregar" id="${producto.id}">Agregar</button>
-                    </div>
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">USD${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
         `;
+
         contenedorProductos.append(div);
     })
+
+    actualizarBotonesAgregar();
 }
 
 cargarProductos(productos);
 
 botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) => {
-        botonesCategorias.forEach(boton => boton.classList.remove("active"));
-        e.currentTarget,classList.add("active");
 
-        if(e.currentTarget,id != "todos") {
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
             const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
             cargarProductos(productosBoton);
-        } else{
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
             cargarProductos(productos);
         }
-    })
-})
 
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
